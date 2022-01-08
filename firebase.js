d15688913a60271c6a2b365a3f2d5f20e74f81f6
medchouaieb <medchouaieb.sayari@ihec.u-carthage.tn>
2022-01-08 10:07:49 +0100

    Day 2 Amazon clone 2.0

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAy5m6Acwh0Aj4HjYfA5xggl4RsJ3SCyv4",
+    authDomain: "clone-b5dab.firebaseapp.com",
+    projectId: "clone-b5dab",
+    storageBucket: "clone-b5dab.appspot.com",
+    messagingSenderId: "852205737108",
+    appId: "1:852205737108:web:788acedc854a5d6a189081"
+  };
